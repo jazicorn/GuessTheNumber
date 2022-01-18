e156325df57a548704e5f7f06f20fe52f9c57ba5
Jasmine Anderson <jazz2900@gmail.com>
2022-01-18 15:42:01 -0500

    created methods to loop through game

--- src/main/java/com/project/Play.java
+++ src/main/java/com/project/Play.java
@@ -11,7 +11,6 @@ public class Play {
 
     // name
     public String name;
-    public String nameReturn;
     public String nameAsk;
 
     // guess
@@ -21,43 +20,31 @@ public class Play {
     public String guessPrompt;
     public String guessLow = "Your guess is too low";
     public String guessHigh = "Your guess is too high";
-    public String guessCorrect;
+    public String guessAgain = "Guess Again";
+    public String guessIncorrect;
     public String congrats;
     public String congrats2;
+    public String gameOver;
 
     // play again
     public boolean replay;
     public String playAgain;
     public char answer;
-    public char y;
     public char n;
-    public int counter = 0;
+    public int counter;
+    public String counterCount = String.format("you have %1$s chances left", counter);
 
     // initiate scanner for user input
     Scanner scan = new Scanner(System.in);
 
     public void run() {
-        // pseudocode
-        // generate random number
-        // user guess the number
-        // after each guess determine if too high or too low
-        // user wins if guess within 6 tries
-
-        // ask username
-        // return instructions with username included
-        // take user input
-        // too high or too low
-        // ask user to guess again
-        // create counter to keep track of user tries
-        // if user guess number they win, ask to play again
-        // if user doesn't guess within 6 tries user looses ask to play again
-        // either way reset counter and generate new random number;
         start();
-
+        setGuess();
     }
 
     // give game instructions and take user's name
     public void start() {
+        counter = 6;
         // ask for name
         nameAsk = "Hello what is your name?";
         System.out.println(nameAsk);
@@ -65,30 +52,13 @@ public class Play {
         name = scan.nextLine();
 
         // return name
-        nameReturn = String.format("\nYour name is: %1$s", name);
-        System.out.println(nameReturn);
+        // nameReturn = String.format("\nYour name is: %1$s", name);
+        // System.out.println(nameReturn);
 
-        // random
-        int random;
-        String randomReturn;
         // generate random number
         random = (int)(Math.random() * 21);
         randomReturn = String.format("Random number generated: %1$s", random);
         System.out.println(randomReturn);
-    }
-
-    // user re-guess random number value;
-    public void newGuess() {
-        // user re-guess
-        if (guessSet > random) {
-            System.out.println(guessHigh);
-            guess = scan.nextInt();
-            guessSet = guess;
-        } else if(guess < random) {
-            System.out.println(guessLow);
-            guess = scan.nextInt();
-            guessSet = guess;
-        }
     }
 
     // set number value for first guess;
@@ -104,25 +74,83 @@ public class Play {
         guessReturn = String.format("You guessed: %1$s", guess);
         System.out.println(guessReturn);
         guessSet = guess;
+        if (guess == random) {
+            winner();
+        }
+        newGuess();
     }
 
-    // congratulate user, confirm random number, & ask to play again
-    public void restart() {
-        // confirm user guess
-        guessCorrect = String.format("Your guess: %1$s", guessSet);
-        System.out.println(guessCorrect);
+    // user re-guess random number value;
+    public void newGuess() {
+        // user re-guess
+        while(counter > 0) {
+            if (guessSet > random) {
+                counter--;
+                System.out.println(guessHigh);
+                System.out.println(guessAgain);
+                guess = scan.nextInt();
+                guessSet = guess;
+                System.out.println("Your new guess: " + guessSet);
+                counterCount = String.format("you have %1$s chances left", counter);
+                System.out.println(counterCount);
+                newGuess();
+            } else if (guessSet < random) {
+                counter--;
+                System.out.println(guessLow);
+                System.out.println(guessAgain);
+                guess = scan.nextInt();
+                guessSet = guess;
+                System.out.println("Your new guess: " + guessSet);
+                counterCount = String.format("you have %1$s chances left", counter);
+                newGuess();
+            } else{
+                counter = 0;
+            }
+        }
+        if (guessSet == random) {
+            winner();
+        }
+        gameOver();
+    }
+
+    // if user picks correct number
+    public void winner() {
+        // reset counter;
+        counter = 6;
         // congratulate user and confirm name
-        congrats = String.format("Good job, %1$s", name);
+        congrats = String.format("Good job, %1$s!!!", name);
+        System.out.println(congrats);
         // confirm generated random number, and loops needed guess number correctly
         congrats2 = String.format("You guessed the number, %1$s, in %2$s guesses!", random, counter);
+        System.out.println(congrats2);
         // ask user to play again
-        playAgain = "would you like to play again? (y or n)";
-        // take in user answer
-        Scanner scan = new Scanner(System.in);
-        answer = scan.next().charAt(0);
-        // set playAgain
-        if (answer == n) {
-            replay = false;
+        restart();
+    }
+
+    // if user runs out of chances
+    public void gameOver() {
+        guessIncorrect = String.format("Sorry, your guess: %1$s, was incorrect", guessSet);
+        gameOver = String.format("The correct answer was: %1$s", random);
+        System.out.println(guessIncorrect);
+        System.out.println(gameOver);
+        restart();
+    }
+
+    // ask user to play again
+    public void restart() {
+        while (replay) {
+            playAgain = "would you like to play again? (y or n)";
+            System.out.println(playAgain);
+            // take in user answer
+            Scanner scan = new Scanner(System.in);
+            answer = scan.next().charAt(0);
+            // set playAgain
+            if (answer == n) {
+                System.out.print("Goodbye!");
+                replay = false;
+            }
+            start();
         }
+        System.exit(0);
     }
 }
