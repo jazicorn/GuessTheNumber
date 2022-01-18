7b558838ae7421932d08fb83cb6f30ea667e448b
Jasmine Anderson <jazz2900@gmail.com>
2022-01-18 11:28:18 -0500

    wrote psuedo code

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,8 +1,58 @@
 package com.project;
 
+import java.util.Scanner;  // Import the Scanner class
+
 public class Main {
+    public static void run() {
+        // generate random number
+        // user guess the number
+        // after each guess determine if too high or too low
+        // user wins if guess within 6 tries
+
+        // ask username
+        // return instructions with username included
+        // take user input
+        // too high or too low
+        // ask user to guess again
+        // create counter to keep track of user tries
+        // if user guess number they win, ask to play again
+        // if user doesn't guess within 6 tries user looses ask to play again
+        // either way reset counter and generate new random number;
+
+        // int random;
+        // boolean playAgain;
+        // int counter;
+        // String name;
+        // String welcome;
+
+        // function user input
+
+        // "Hello what is your name?
+        // input {Jasmine}
+
+        // function random number
+
+        // "Well, {Jasmine}, I am thinking of a number 1 and 20".
+        // Take a guess;
+
+        // Your guess is too low.
+        // Take a guess
+
+        // Your guess is too high.
+        // Take a guess
+
+        // function try again
+
+        // Good job, {Jasmine}! You guessed my number in {counter} guesses!
+        // would you like to play again? (y or n)
+
+
+    }
 
     public static void main(String[] args) {
+
+        run();
         System.out.println("Hello");
+
     }
 }
